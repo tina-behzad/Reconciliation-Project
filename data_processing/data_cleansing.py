d00af32d1dd8ec40306c7da5441da0770383dca2
Tina Behzad <ti.be1376@yahoo.com>
2024-01-31 21:07:53 -0500

    Data sets added. Adult data cleaning method added.

---
+++ data_processing/data_cleansing.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+def adult_data_cleansing():
+    target_column = 'income'
+    data = pd.read_csv('../data/adult.csv')
+    data[target_column] = (data[target_column] == '>50K').astype(int)
+    data = data[~data.apply(lambda row: ' ?' in row.values, axis=1)]
+    data = data.drop(['fnlwgt', 'capital_loss', 'capital_gain', 'education'], axis=1)
+    data = data.replace(r'\s', '', regex=True)
+    category_mapping = {
+        'Private': 'private',
+        'Self-emp-not-inc': 'self',
+        'Self-emp-inc': 'self',
+        'Federal-gov': 'gov',
+        'Local-gov': 'gov',
+        'State-gov': 'gov',
+        'Without-pay': 'other',
+        'Never-worked': 'other'
+    }
+    data['workclass'] = data['workclass'].replace(category_mapping)
+    mask = data['marital_status'].str.contains('Married', case=True)
+    data.loc[mask, 'marital_status'] = 'Married'
+    data.to_csv('../data/adult_cleaned.csv')
+
+
+def compasss_data_cleansing():
+    data = pd.read_csv('../data/compas.csv')
+    data = data[(data["days_b_screening_arrest"] <= 30)
+                & (data["days_b_screening_arrest"] >= -30)
+                & (data["is_recid"] != -1)
+                & (data["c_charge_degree"] != 'O')
+                & (data["score_text"] != 'N/A')].reset_index(drop=True)
+    data.to_csv('../data/compass_cleaned.csv')
+
+adult_data_cleansing()
