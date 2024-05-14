18560cc8d9f81f1c656228a8aecc35ad04e88c7b
tina515 <30392973+tina515@users.noreply.github.com>
2024-05-14 13:30:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from sklearn.naive_bayes import GaussianNB
 from pipeline import Pipeline
 
 #possible models
-#classifiers: (changes need to be made to modelwrapper for them to work)
 # LogisticRegression
 # RandomForestClassifier
+#DecisionTreeClassifier
 #regressions
-# 'Tree' for DecisionTreeRegressor
-#'LinearRegression' for linear regression
+# DecisionTreeRegressor
+# LinearRegression
+#KNeighborsRegressor
 #possible approaches:
-#same model classifier is a string
-#different features classifier is a string
-#same data classifier is a list
+#'same model' classifier is a string, different subsets of data will be used
+#'different features' classifier is a string
+#'same data' classifier is a list, different models will be used
 if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read('configs.ini')
@@ -23,10 +24,3 @@ if __name__ == "__main__":
     pipeline = Pipeline('Compas_Data', config, 'same model', 'LogisticRegression')
     pipeline.run()
 
-
-# KNeighborsClassifier
-# DecisionTreeClassifier
-# LogisticRegression
-# LinearRegression
-# DecisionTreeRegressor
-# KNeighborsRegressor
